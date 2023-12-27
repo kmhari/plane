b603d7dd5ffe199dc6a291e05f7a926d8b1dcb9d
Aravindan Udayasekaran <aravindan@f22labs.com>
2023-12-28 00:14:07 +0530

    signals added for calculate_consumed_hours

---
+++ apiserver/plane/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import *
+from django.dispatch import receiver
+from plane.db.models import IssueActivity, Issue
+import time
+
+@receiver(post_save, sender=IssueActivity)
+def calculate_consumed_hours(instance, created, **kwargs):
+
+    try:
+        print("New State : {}".format(instance.old_value))
+        issue_id = instance.issue_id
+        if created and instance.old_value == 'In Progress':
+            issue = Issue.objects.get(pk=issue_id)
+            obj = IssueActivity.objects.filter(issue_id = issue_id, new_value='In Progress').last()
+            issue.time_consumed = issue.time_consumed + int(((int(time.time()) - obj.epoch) / 60))
+            issue.save()
+            print("Time Consumed updated to {} for Issue {}".format(issue.time_consumed, instance.issue_is))
+    except Exception as e:
+        print("Exception {} occured in consumed hours.".format(str(e)))
+
+
